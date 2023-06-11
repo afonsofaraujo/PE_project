79ccf416b806b368e1a129583ed7da8fa5c68878
Afonso Araújo <102881461+afonsofaraujo@users.noreply.github.com>
2023-06-11 17:07:51 +0100

    Rever 8

--- 10_projeto_PE.R
+++ 10_projeto_PE.R
@@ -1,17 +1,16 @@
 set.seed(1194)
-m <- 100                # Número de amostras
-n <- 45                 # Tamanho da amostra
-mu_null <- 78.1         # Valor esperado sob a hipótese nula
-mu_alternative <- 79.1  # Valor esperado sob a hipótese alternativa
-sigma2 <- 4             # Variância
-alpha <- 0.1            # Nível de significância
-# Geração das amostras
-samples <- matrix(rnorm(m * n, mean = mu_alternative, sd = sqrt(sigma2)), ncol = n)
-# Estatísticas de teste
-test_statistics <- apply(samples, 1, function(x) sqrt(n) * (mean(x) - mu_null) / sqrt(sigma2))
-test_statistics
-# Contagem de não rejeição da hipótese nula
+m <- 100                # number of samples
+n <- 45                 # size of samples
+h_null <- 78.1          # expected value h_null
+h_alt <- 79.1           # expected value h_alt
+sigma2 <- 4             # Variance
+alpha <- 0.1            # significance level
+# Sample generation
+samples <- matrix(rnorm(m * n, mean = h_alt, sd = sqrt(sigma2)), ncol = n)
+# Test statistics
+test_statistics <- apply(samples, 1, function(x) sqrt(n) * (mean(x) - h_null) / sqrt(sigma2))
+test_statistics         # show test statistics 
 non_rejection_count <- sum(abs(test_statistics) <= qnorm(1 - (alpha/2)))
-qnorm(1 - (alpha/2)) # Valor limite
+qnorm(1 - (alpha/2))    # limit
 probability_non_rejection <- non_rejection_count / m
 probability_non_rejection
