f36f92c55a7de907a343d92bc217bcc57e0fc813
Afonso Araújo <102881461+afonsofaraujo@users.noreply.github.com>
2023-06-15 16:28:02 +0100

    update 8

--- 8_projeto_PE.R
+++ 8_projeto_PE.R
@@ -9,46 +9,26 @@ sd <- sqrt(2)
 # Cauchy distribution
 cau <- rcauchy(n, loc, sca)
 sorted_cauchy <- sort(cau)
-# Normal distribution
-nor <- rnorm(n, nu, sd)
-sorted_normal <- sort(nor)
+
+
 quantis_cauchy <- numeric(n)
 for (i in 1:n){
   quantis_cauchy[i] <- quantile(sorted_cauchy, i/(n+1))
 }
-quantis_cauchy
+
+nor <- rnorm(n, nu, sd)
 quantis_normal <- numeric(n)
 for (j in 1:n){
-  quantis_normal[j] <- quantile(sorted_normal, j/(n+1))
+  quantis_normal[j] <- quantile(nor, j/(n+1))
 }
-quantis_normal
-# Criar dataframe dos valores ordenados e quantis para Cauchy
-df_cauchy <- data.frame(Quantiles = quantis_cauchy, Valores = sorted_cauchy)
-# Criar dataframe dos valores ordenados e quantis para Normal
-df_normal <- data.frame(Quantiles = quantis_normal, Valores = sorted_normal)
-# Plot
+
+df <- data.frame(Quantiles_cau = quantis_cauchy, Quantiles_nor = quantis_normal, Valores = sorted_cauchy)
+
+
 ggplot() +
-  geom_line(data = df_cauchy, aes(x = Quantiles, y = Valores), color = "blue", size = 0.5) +
-  geom_line(data = df_normal, aes(x = Quantiles, y = Valores), color = "red", size = 0.8) +
+  geom_point(data = df, aes(x = Valores, y = Quantiles_cau), color = "blue") +
+  geom_point(data = df, aes(x = Valores, y = Quantiles_nor), color = "red") +
   geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
-  xlab("Quantis de probabilidade") +
-  ylab("Valores gerados ordenados") +
+  xlab("Valores gerados ordenados") +
+  ylab("Quantis de probabilidade") +
   labs(title = "Distribuições") 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
